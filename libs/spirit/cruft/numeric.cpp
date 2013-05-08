67008a03015cb730304dd5546588bdfe1d3a4c9f
Alex Dubov <oakad@yahoo.com>
2013-05-09 03:10:35 +1000

    boost::spirit: some test harness functions for numeric parser
    Signed-off-by: Alex Dubov <oakad@yahoo.com>

--- libs/spirit/cruft/numeric.cpp
+++ libs/spirit/cruft/numeric.cpp
@@ -9,6 +9,10 @@
 #include <boost/spirit/home/qi/numeric/uint.hpp>
 #include <boost/spirit/home/qi/numeric/int.hpp>
 
+#include <boost/spirit/include/karma.hpp>
+
+#include <boost/iterator/function_input_iterator.hpp>
+
 #include <boost/spirit/repository/home/qi/numeric/numeric.hpp>
 
 #include <boost/mpl/x11/map.hpp>
@@ -16,29 +20,30 @@
 #include <boost/random/random_device.hpp>
 
 namespace qi = boost::spirit::qi;
+namespace karma = boost::spirit::karma;
 namespace qi_repo = boost::spirit::repository::qi;
 
 namespace x11 = boost::mpl::x11;
 
 template <typename T>
 void make_numbers(
-	std::vector<T> &out, size_t count, unsigned int min_digits,
-	unsigned int max_digits, std::basic_string<T> const &delim
+	std::vector<T> &out, size_t count,
+	std::basic_string<T> const &delim
 )
 {
-	std::vector<T> single(max_digits);
+	using karma::uint_;
+	using karma::lit;
+
 	boost::random_device r;
-	int c_dig(0);
 
-	for (size_t c(0); c < count; ++c) {
-		single.clear();
-
-		for (int d(0); d < max_digits; ++d) {
-			int x(r());
-			if (10 > (x & 0xf))
-				
-		}
-	}
+	karma::generate(
+		std::back_inserter(out),
+		uint_ % lit(delim),
+		boost::make_iterator_range(
+			boost::make_function_input_iterator(r, size_t(0)),
+			boost::make_function_input_iterator(r, count)
+		)
+	);
 }
 
 struct to_decimal {
@@ -54,48 +59,69 @@ struct to_decimal {
 };
 
 template <typename T>
-void test_vector(std::string s)
+void test_vector(std::vector<T> &out, std::vector<char> const &in)
 {
 	using boost::spirit::standard::digit_type;
 	using boost::spirit::standard::space_type;
 
-	std::vector<T> res;
 	typedef x11::map<
 		x11::pair<qi_repo::with_extractor, digit_type>,
 		x11::pair<
 			qi_repo::with_inserter,
 			qi_repo::detail::default_inserter<T>
-		>//,
-//		x11::pair<qi_repo::with_filter, to_decimal>
+		>
 	> int_policy;
 	qi_repo::numeric_parser<T, int_policy> uint1;
 
-	qi::rule<std::string::iterator, std::vector<T>(), space_type> p(
-		uint1 % ','
-		//boost::spirit::ushort_ % ','
-	);
+	qi::rule<
+		std::vector<char>::const_iterator, std::vector<T>(), space_type
+	> p(uint1 % ',');
 
-	bool rv(qi::phrase_parse(s.begin(), s.end(), p, space_type(), res));
+	bool rv(qi::phrase_parse(in.begin(), in.end(), p, space_type(), out));
+}
 
-	std::cout << typeid(T).name() << ": rv: " << rv << '\n';
-	bool delim(false);
-	for(T const &v: res) {
-		if (delim)
-			std::cout << ", ";
+template <typename T>
+void test_vector_ref(std::vector<T> &out, std::vector<char> const &in)
+{
+	using boost::spirit::standard::space_type;
+	using qi::uint_;
 
-		delim = true;
-		std::cout << long(v);
-	}
-	std::cout << '\n';
+	qi::rule<
+		std::vector<char>::const_iterator, std::vector<T>(), space_type
+	> p(uint_ % ',');
+
+	bool rv(qi::phrase_parse(in.begin(), in.end(), p, space_type(), out));
 }
 
 int main(int argc, char **argv)
 {
-	std::string t_str("30000, 32767, 32768, 32769, 65535, 65536, 65537");
-	test_vector<char>(t_str);
-	test_vector<unsigned char>(t_str);
-	test_vector<short>(t_str);
-	test_vector<unsigned short>(t_str);
+	using karma::uint_;
+	using karma::lit;
+
+	int count(5);
+
+	std::vector<char> rnd_str;
+	make_numbers(rnd_str, count, std::string(", "));
+
+	std::vector<unsigned int> res1, res2;
+	res1.reserve(count);
+	res2.reserve(count);
+
+	test_vector<unsigned int>(res1, rnd_str);
+	test_vector_ref<unsigned int>(res2, rnd_str);
+	rnd_str.push_back(0);
+	std::cout << "in: " << &rnd_str.front() << '\n';
+	karma::generate(
+		std::ostream_iterator<char>(std::cout),
+		lit("o1: ") << (uint_ % lit(", ")) << lit('\n'),
+		res1
+	);
+
+	karma::generate(
+		std::ostream_iterator<char>(std::cout),
+		lit("o2: ") << (uint_ % lit(", ")) << lit('\n'),
+		res2
+	);
 
 	return 0;
 }
